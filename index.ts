6a2c3616cbc62e7f14104301fd8db6b325d84fd0
Plopmenz <plopmenz@gmail.com>
2024-05-21 16:15:27 +0200

    save memory values on interupt

--- index.ts
+++ index.ts
@@ -60,12 +60,17 @@ async function start() {
     }
   });
 
-  process.on("SIGINT", function () {
+  process.on("SIGINT", async () => {
     console.log("Stopping...");
 
     multichainWatcher.forEach((contractWatcher) => {
       contractWatcher.stopAll();
     });
+    await Promise.all(
+      Object.values(storage).map((storageItem) => {
+        storageItem.update(() => {}); // Save all memory values to disk
+      })
+    );
     process.exit();
   });
 
